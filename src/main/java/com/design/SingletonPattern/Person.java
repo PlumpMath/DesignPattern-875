0bc210a4f1d2ef10b444967cc48b9be29d210a05
sahil kohli <sahilk@cvent.com>
2016-11-06 02:42:36 +0530

    Singleton pattern added

---
+++ src/main/java/com/design/SingletonPattern/Person.java
@@ -0,0 +1,30 @@
+package com.design.SingletonPattern;
+
+/**
+ * Created by sahilk on 06/11/16.
+ */
+public class Person {
+
+    // Always pull value from head not from thread stack
+    private static volatile Person person;
+
+    private Person(){
+    }
+    // Double check locking
+    public static Person getInstance(){
+        if (person == null) {
+            synchronized (Person.class) {
+                if (person == null) {
+                    person = new Person();
+                    return person;
+                }
+            }
+        }
+        return person;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getInstance());
+        System.out.println(getInstance());
+    }
+}
